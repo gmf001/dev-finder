1b6f08bc76725f29bb81d6d7393f692fd8a542ab
Glenn Francis <xglennfrancisx@gmail.com>
2023-01-18 22:49:23 +1000

    logging error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 import PageHeading from '@/components/PageHeading';
 import UserDetails from '@/components/UserDetails';
 import SearchForm from '@/components/SearchForm';
-import { GithubUser } from '@/types';
+import github from '@/lib/github';
 
 export const revalidate = 600;
-
-async function getData(username: string): Promise<GithubUser | null> {
-  const res = await fetch(`https://api.github.com/users/${username}`, {
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-      Authorization: `token ${process.env.GITHUB_AUTH_TOKEN}`
-    }
-  });
-  if (!res.ok) return null;
-  return res.json();
-}
+export const dynamic = 'force-dynamic';
 
 interface Props {
   searchParams: {
@@ -23,17 +13,13 @@ interface Props {
 }
 
 export default async function Home({ searchParams }: Props) {
-  const userData = await getData(searchParams.username);
-  console.log('data', userData);
+  const userData = await github(searchParams.username);
 
   return (
     <main className='grid h-screen place-items-center'>
       <div className='container w-full max-w-3xl py-10 space-y-6'>
         <PageHeading />
         <SearchForm />
-        <pre>{JSON.stringify(process.env.GITHUB_AUTH_TOKEN, null, 2)}</pre>
-        <pre>{JSON.stringify(searchParams.username, null, 2)}</pre>
-        <pre>{JSON.stringify(userData, null, 2)}</pre>
         {userData ? <UserDetails user={userData} /> : null}
       </div>
     </main>
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,17 +1,14 @@
 import type { GithubUser } from '@/types';
 
 const github = async (username: string) => {
-  console.log('token', process.env.GITHUB_AUTH_TOKEN);
   const res = await fetch(`https://api.github.com/users/${username}`, {
     headers: {
       Accept: 'application/vnd.github.v3+json',
       Authorization: `token ${process.env.GITHUB_AUTH_TOKEN}`
     }
   });
-  if (!res.ok) {
-    return null;
-    // throw new Error('Failed to fetch data');
-  }
+
+  if (!res.ok) return null;
   const userData = await res.json();
   return userData as GithubUser;
 };
